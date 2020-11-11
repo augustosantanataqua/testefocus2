2db1cfe58e381190082814762ccc06a78662ecbd
augustosantanataqua <74194181+augustosantanataqua@users.noreply.github.com>
2020-11-11 10:45:53 -0300

    Add files via upload

---
+++ TESTEE.sql
@@ -0,0 +1,11 @@
+select * from categorias
+
+update categorias set descricao = 'bazar alterado' where categoriaid = 2 
+
+
+insert into categorias (descricao) values ('Notebooks'),('Tablets'), ('Roteadores'), ('Periféricos')
+
+DBCC CHECKIDENT('categorias', reseed, 0)
+
+sp_helpdb vendas
+sp_help categorias
